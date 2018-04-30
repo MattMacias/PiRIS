91ee2688cbd54d15fcb2259ccefb2b93f6bc31de
MattMacias <37856344+MattMacias@users.noreply.github.com>
2018-04-29 23:45:12 -0500

    Some basic setup stuff

--- Main.py
+++ Main.py
@@ -3,17 +3,32 @@
 #
 #
 ##########################################################
-
-
-# GPIO Setup
-
-
-
-
+print "Loading files"
+import pygame
+import sys
+print "Files ready"
 
 # GUI Setup
+pygame.init()
 
+def newWindow():
+    global window, winHeight, winWidth, winTitle
+    winWidth, winHeight = 800,600
+    winTitle = "PiRIS Home"
+    pygame.display.set_caption(winTitle)
+    window = pygame.display.set_mode((winWidth, winHeight), pygame.HWSURFACE|pygame.DOUBLEBUF)
 
+def fpsCounter():
+    global curSec, curFrame, FPS
+
+    if curSec == time.strftime("%S"):
+        curFrame += 1
+    else:
+        FPS = curFrame
+        curFrame = 0
+        curSec = time.strftime("%S")
+
+# GPIOSetup
 
 
 
@@ -28,3 +43,27 @@
 
 
 # Main Section, establishes the GUI and GPIO functions
+#Variable setup
+isRunning = True
+curSec = 0
+curFrame = 0
+FPS = 0
+
+# Init the Home Screen
+newWindow()
+
+
+while isRunning:
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            isRunning = 0
+
+    # Render the next frame
+    window.fill((0,0,0))
+
+    
+    pygame.display.update()
+
+
+pygame.quit()
+sys.exit()
---
+++ NAMELIST.py
@@ -0,0 +1,47 @@
+from random import randint
+
+def randNAME():
+    return Names[randint(0,len(Names)-1)]
+
+#Core list of names.
+# Add a name by just appending to Names
+Names = []
+Names.append("Anky")
+Names.append("Adam")
+Names.append("Andrew")
+Names.append("Bill")
+Names.append("Chuck")
+Names.append("Dave")
+Names.append("Emmett")
+Names.append("Frank")
+Names.append("Gary")
+Names.append("Harry")
+Names.append("Isaac")
+Names.append("James")
+Names.append("Kameron")
+Names.append("Luke")
+Names.append("Murphy")
+Names.append("Nate")
+Names.append("Owen")
+Names.append("Paul")
+Names.append("Quincy")
+Names.append("Ron")
+Names.append("Steve")
+Names.append("Tyler")
+Names.append("Vance")
+Names.append("Wallace")
+Names.append("Yorick")
+Names.append("Zeph")
+Names.append("Alice")
+Names.append("Bonnie")
+Names.append("Cammie")
+Names.append("Doris")
+Names.append("Eve")
+Names.append("Flo")
+
+
+
+
+
+
+             
