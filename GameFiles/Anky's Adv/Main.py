b8f12502b577b9661fb6e8e89659d8652d2d55ec
Gavin Phillips <38142343+gph006@users.noreply.github.com>
2018-05-16 05:42:51 -0500

    fixed hitboxes

--- GameFiles/Anky's Adv/Main.py
+++ GameFiles/Anky's Adv/Main.py
@@ -13,7 +13,7 @@ WIDTH = 800
 HEIGHT = 416
 
 
-pygame.display.set_caption("RPG Demo")
+pygame.display.set_caption("Anky's Adventure")
 
 ##
 bg = pygame.image.load(("./GameFiles/Anky's Adv/background.gif"))
@@ -113,16 +113,9 @@ gameRunning = True
 
 while gameRunning:
     
-    #Size is a square length/width
-    #size = 10
-    #headx = winWidth  /2 - size/2
-    #heady = winHeight /2 - size/2
-    #goingUp = 0
-    #goingRight = 0
-    #upSpeed = 3
-    #rightSpeed = 3
+
     playing = True
-    #looking = "North"
+   
     arrows = pygame.key.get_pressed()[273:277]
     while playing:
         for event in pygame.event.get():
@@ -138,18 +131,6 @@ while gameRunning:
                     playing = False
             arrows = pygame.key.get_pressed()[273:277]
 
-##            if (event.type == pygame.KEYDOWN):
-##                if event.key    == pygame.K_UP:
-##                    looking = "North"
-##                elif event.key  == pygame.K_DOWN:
-##                    looking = "South"
-##                elif event.key  == pygame.K_RIGHT:
-##                    looking = "East"
-##                elif event.key  == pygame.K_LEFT:
-##                    looking = "West"
-            ##elif event.ty == pygame.K_ESCAPE:
-               # gameRunning = False
-##                    playing = False
 
         all_sprites.update()
 
@@ -166,67 +147,24 @@ while gameRunning:
         if hits:
             gameRunning = False
 
-##        # Up Arrow
-##        if (arrows[0] == 1):
-##            if (goingUp < 9):
-##                goingUp += 1
-##        # Down Arrow, Up overrides if both are pressed
-##        elif (arrows[1] == 1):
-##            if (goingUp > -9):
-##                goingUp -= 1
-##        elif (goingUp < 0):
-##            goingUp += 1
-##        elif (goingUp > 0):
-##            goingUp -= 1
-##        # Right Arrow
-##        if (arrows[2] == 1):
-##            if (goingRight <9):
-##                goingRight += 1
-##        # Left Arrow, Right overrides if both are pressed
-##        elif (arrows[3] == 1):
-##            if (goingRight > -9):
-##                goingRight -= 1
-##        elif (goingRight > 0):
-##            goingRight -= 1
-##        elif (goingRight < 0):
-##            goingRight += 1
-##        print arrows, goingUp, goingRight, looking
-            
-                
-        # Movement of the player
-        #if (headx < winWidth-size and goingRight >0):
-           # headx += goingRight
-        #elif (headx > 0 and goingRight < 0):
-            #headx += goingRight
-        ##if (heady > 0 and goingUp > 0):
-           # heady -= goingUp
-        #elif (heady < winHeight-size and goingUp < 0):
-           # heady -= goingUp
-            
-        #print heady
 
 
 
         # Displays the background
         gameDisplay.blit(bg, [0, 0])
 
-        # Draws placeholder gridlines
-        for meridian in range(0,winWidth,32):
-            gameDisplay.fill(black, rect = [meridian, 0, 1, winHeight])
-        for parallel in range(0,winHeight,32):
-            gameDisplay.fill(black, rect = [0,parallel, winWidth, 1])
 
 
-        #all_sprites.update()
+
+    
 
         all_sprites.draw(gameDisplay)
         drawText(gameDisplay, str(score), 18, WIDTH/2, 30)
         pygame.display.flip()
         
-        ##
+ 
 
-        # Draw the player
-       # gameDisplay.blit(player, (headx, heady))
+
         sleep(0.016)
         pygame.display.update()
     
